fd0bc8395ddbab39234069f5ea1cf8fd7cb07cb1
wellynton.spagnol <wellyntonspagnol10@gmail.com>
2024-09-11 08:41:04 -0300

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import { ButtonContainer, type ButtonVariant } from "./Button.style"
+
+interface ButtonProps{
+  variant?: ButtonVariant;
+}
+
+export function Button({variant = 'primary'}: ButtonProps){
+  return(
+    <ButtonContainer variant={variant}>Enviar</ButtonContainer>
+  )
+}
